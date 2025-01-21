4d3bca01c70cc905ebc9998cb93cc401889254e2
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-21 21:45:13 +0200

    Delegate_Ex:1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,11 +1,21 @@
 ﻿namespace Demo
 {
+    public delegate int StringFun(string str);
     internal class Program
     {
         static void Main()
         {
-            #region MyRegion
-
+            #region Delegate_Ex:1
+            //StringFun fun = new StringFun(String_Functions.getCountUpperChars); // Syntax Suger
+            //StringFun fun = String_Functions.getCountUpperChars;
+            ////int result = fun.Invoke("Omar Saad");
+            //int result = fun("Omar Saad"); // Syntax Suger
+            //Console.WriteLine(result); 
+            //StringFun fun = new StringFun(String_Functions.getCountLowererChars); // Syntax Suger
+            StringFun fun = String_Functions.getCountLowererChars;
+            int result = fun.Invoke("Omar Saad");
+            //int result = fun("Omar Saad"); // Syntax Suger
+            Console.WriteLine(result);
             #endregion
         }
     }
